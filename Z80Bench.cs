66b4bfdbd52db994cdc8889712ee46de5102cae3
Maxim <maxim@smspower.org>
2020-08-26 04:06:37 +0100

    Remove unused references

--- Z80Bench.cs
+++ Z80Bench.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Text;
 using Konamiman.Z80dotNet;
 
 namespace z80bench
