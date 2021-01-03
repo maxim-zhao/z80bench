fc90cc6bdafc412fad4604ea84b77168d547d382
Dread Pirate Johnny Spaceboots <dpjspaceboots@bypass.systems>
2021-01-03 00:36:26 -0330

    Added another CLI test and the first runner test

---
+++ tests/z80benchtests/BenchmarkRunnerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace z80bench.tests {
+    [TestClass]
+    public class BenchmarkRunnerTests {
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Emulator stopped due to running for too long")]
+        public void ThrowsExceptionWhenOverMaxCycles() {
+            var benchmark = new Benchmark();
+            var data = new z80bench.Benchmark.Data();
+            var memory = new List<Benchmark.Data>();
+            data.Values = new byte[6] { 0x3E, 0x07, 0xC6, 0x04, 0x3C, 0xC9 };
+            data.Offset = 0;
+            memory.Add(data);
+
+            benchmark.MaxCycles = 1;
+            benchmark.Memory = memory;
+
+            new BenchmarkRunner(benchmark);
+        }
+    }
+}
--- tests/z80benchtests/CliTests.cs
+++ tests/z80benchtests/CliTests.cs
@@ -7,38 +7,59 @@ using System.Linq;
 namespace z80bench.tests {
     [TestClass]
     public class CliTests {
+        private StringWriter _stdOut;
+        private StringWriter _stdErr;
+        private static readonly string[] _parameters = {
+            "--execute=<address>",
+            "--stack-pointer=<address>",
+            "--max-cycles=<count>",
+            "--vram-compare=<filename>[@<offset>]"
+        };
+
+        [TestInitialize()]
+        public void Setup() {
+            // Override console standard and error output streams before each test
+            _stdOut = new StringWriter();
+            _stdErr = new StringWriter();
+            Console.SetOut(_stdOut);
+            Console.SetError(_stdErr);
+        }
+
         [TestMethod]
-        public void ExitsWithErrorWithoutArguments()
-        {
+        public void ExitsWithErrorWithoutArguments() {
             // Execute with no parameters and collect the output
             int result;
             List<string> output;
-            using (var sw = new StringWriter())
-            {
-                Console.SetOut(sw);
-                Console.SetError(sw);
-                result = Z80Bench.Main(new string[] { });
-                output = sw.ToString()
-                    .Split("\n")
-                    .Select(x => x.Trim())
-                    .ToList();
-            }
+            result = Z80Bench.Main(new string[] { });
+            output = _stdOut.ToString()
+                .Split("\n")
+                .Select(x => x.Trim())
+                .ToList();
 
             // Expect -1 for an error
             Assert.AreEqual(result, -1);
-            
+
             // Expect the first line to be usage info
             Assert.AreEqual(output[0], "Usage: z80bench <filename[@address]> [additional files] [options]");
 
             // Expect the various parameters to be in the usage info
-            foreach (var line in new[] {
-                "--execute=<address>",
-                "--stack-pointer=<address>",
-                "--max-cycles=<count>",
-                "--vram-compare=<filename>[@<offset>]"
-            }) {
+            foreach (var line in _parameters) {
                 Assert.IsTrue(output.Any(x => x.Contains(line)));
             }
+        }
+
+        [TestMethod]
+        public void ExitsWithErrorWithUnknownArgument() {
+            // Execute with unknown parameter and collect output
+            int result = Z80Bench.Main(new string[] { "--unknown-arg", "unknownValue" });
+            string output = _stdOut.ToString();
+
+            // Expect -1 for an error
+            Assert.AreEqual(-1, result);
+
+            // Expect an errors section and for it to contain a message about the unknown parameter
+            Assert.IsTrue(output.Contains("Errors:"));
+            Assert.IsTrue(output.Contains("Option 'unknown-arg' is unknown."));
         }
     }
 }
